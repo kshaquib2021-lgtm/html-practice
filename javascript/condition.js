617068f257a2fa707443a55d2811791170385cca
kshaquib2021-lgtm <kshaquib2021@gmail.com>
2026-08-04 18:30:46 +0530

    javascript.js

---
+++ javascript/condition.js
@@ -0,0 +1,12 @@
+num =45
+
+if(num % 2 ===0){
+    console.log('even');
+    let msg = 'hello';
+    const pi=3.14;
+    
+    pi=456;
+} else {
+    console.log('odd');
+}
+console.log(msg);
---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+console.log("Hello JavaScript Again");
+
+a=20;
+
+console.log(a);
+
+console.log(typeof a);
+
+username ="Shaquib"
+
+console.log(typeof username)
+
+isHungary = false;
+
+console.log(typeof isHungary)
